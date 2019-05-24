e47b15cad3ea85b6f95fe762b5626bd2fcbaecfa
Bonefest <tereshenko1@ukr.net>
2019-05-24 16:20:57 +0300

    Fix

--- tests/myScene.h
+++ tests/myScene.h
@@ -22,12 +22,12 @@ class myScene : public Scene{
 public:
 	myScene():shiftX(0) {
 		//s4block = new Sprite(Game::getMediaManager()->loadTexture("tests/block.png"), (Rect){700, 0}, (Rect){32, 900});
-		label = new Label(Game::getMediaManager()->loadFont("resources/default.ttf",36),"TEST",(Rect){150,150},(SDL_Color){255,255,255});
+		//label = new Label(Game::getMediaManager()->loadFont("resources/default.ttf",36),"TEST",(Rect){150,150},(SDL_Color){255,255,255});
 
 		cam = new Camera();
 
 
-		addSprite(label);
+		//addSprite(label);
 
 		addCamera("camera", cam);
 	}
@@ -36,10 +36,11 @@ public:
 		Scene::controller(event);
 		if(event->type == SDL_KEYDOWN) {
 			const char* key = SDL_GetKeyName(event->key.keysym.sym);
-			if(strlen(key) <= 2 && isalpha(key[0])) {
+			if(strlen(key) <= 2 && isprint(key[0])) {
 				Label* sym =  new Label(Game::getMediaManager()->loadFont("resources/default.ttf",36),SDL_GetKeyName(event->key.keysym.sym),(Rect){double(shiftX),0},(SDL_Color){255,255,255});
 				sym->physics = new Physics(sym,true,1.0,1.0);
 				addSprite(sym);	
+				symbols.push_back(sym);
 
 				shiftX = (shiftX + 48) % SCREEN_WIDTH;
 			}
@@ -48,9 +49,12 @@ public:
 
 	void update(double delta) {
 		Scene::update(delta);
+		std::cout << symbols.size() << std::endl;
 		for(auto symIter = symbols.begin();symIter != symbols.end();) {
 			if((*symIter)->getPosition().y > SCREEN_HEIGHT + SCREEN_HEIGHT/15) {
 				removeSprite(*symIter);
+				delete (*symIter)->physics;
+				delete (*symIter);
 				symIter = symbols.erase(symIter);
 			} else
 				symIter++;
